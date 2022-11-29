b32540eab3472fe4a685d51706b044ba646c1d20
Sean Mckeown <seanm3156@gmail.com>
2022-11-29 19:41:33 +0000

    Added a new page for programing projects

--- website.py
+++ website.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
 @app.route("/home/")
 @app.route("/")
 def home():
-    return render_template("home.html", title='home', num=100)
+    return render_template("home.html", title='Home')
 
 @app.route("/about/")
 def about():
     return render_template("about.html", title='About')
 
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html", title = 'Projects')
+
 if '__main__' == __name__:
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5001)
